1393a8fcf786063f8c5064eb4ca80f0689f245f1
RRFIM <saswswap@gmail.com>
2025-10-02 14:47:31 +0200

    updated burger and finshed modalScroll

--- 4 - menuburger/script.js
+++ 4 - menuburger/script.js
@@ -1,5 +1,16 @@
-
 // 1. Je recupere le burger
 const burger = document.querySelector(".burger");
 // 2. Je recupere la fenetre modal
-const modal = document.querySelector(".modal");
+const modal = document.querySelectorAll(".modal");
+
+burger.addEventListener("click", function(){
+    Moveleft()
+})
+
+function Moveleft()
+{
+    modal.forEach(modal_element => {
+            modal_element.classList.toggle("show-modal")
+        });
+}
+
